c8a6ff5b305324896e77ec527540e42c51b3894a
Adriana Monteiro <adriianamonteiiro@gmail.com>
2023-02-14 19:45:48 -0300

    Adição teste criar usuario com sucesso

--- src/test/java/com/restAssured/AppTest.java
+++ src/test/java/com/restAssured/AppTest.java
@@ -1,13 +1,21 @@
 package com.restAssured;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-public class AppTest 
-{
+public class AppTest {
+
+    @BeforeClass
+    public static void setup(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
     @Test
     public void testeListaDadosUsuarios() {
         when().
@@ -16,5 +24,17 @@ public class AppTest
              statusCode(HttpStatus.SC_OK).
              body("page",is(2)).
              body("data",is(notNullValue()));
+    }
+
+    @Test
+    public void testeCriarUsuarioComSucesso(){
+        given().
+              contentType(ContentType.JSON).
+              body("{\"name\": \"ana\", \"job\": \"eng teste\"}").
+        when().
+              post("https://reqres.in/api/users").
+        then().
+              statusCode(HttpStatus.SC_CREATED).
+              body("name", is("ana"));
     }
 }
